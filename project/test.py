cf3f4c20e93196d886e2195e8028ec2db9065ba7
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-09-04 12:18:35 +0200

    file used to preetier the data

--- project/test.py
+++ project/test.py
@@ -4,10 +4,13 @@ import json
 with open("data-operations.json", "r") as json_file:
     data = json.load(json_file)
 
-for listing in data:
-    listing["Rodzaj zabudowy"] = listing["Rodzaj zabudowy"].strip()
-    # try: listing["Powierzchnia"] = int(listing["Powierzchnia"])
-    # except: listing["Powierzchnia"] = float(listing["Powierzchnia"])
+# for listing in data:
+#     if listing["Liczba pokoi"] == "Kawalerka":
+#         listing["Liczba pokoi"] = 1
+#     else:
+#         listing["Liczba pokoi"] = listing["Liczba pokoi"][0]
+#         listing["Liczba pokoi"] = int(listing["Liczba pokoi"])
+#     print(listing["Liczba pokoi"])
 
 with open("data-operations.json", "w") as json_file:
-    json.dump(data, json_file, indent=2)
+    json.dump(data, json_file, indent=2, sort_keys=True)
