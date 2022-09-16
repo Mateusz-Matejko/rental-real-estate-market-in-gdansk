2c2091a551719c93a2875007aa8ab855ea51b3b9
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-09-16 22:38:50 +0200

    fix made within file from 3rd september

--- project/collected september 3rd/cleared-data/clear-file.py
+++ project/collected september 3rd/cleared-data/clear-file.py
@@ -3,15 +3,18 @@ import json
 with open("../../calculations/sep3.json", "r") as json_file:
     data = json.load(json_file)
 
+number = 1
 for listing in data:
-    if listing["level"] == None:
-        print(listing)
+    listing["listing_no"] = number
+    number += 1
     # if "level" not in listing:
     #     print(listing)
+    print(listing)
 
-# try:
-#     with open("../../calculations/sep3.json", "w") as json_file:
-#         json.dump(data, json_file, indent=2, sort_keys=True)
-# except:
-#     print("Unsuccessfully written")
-# print("Success")
+
+try:
+    with open("cleared-data-3sep.json", "w") as json_file:
+        json.dump(data, json_file, indent=2, sort_keys=True)
+except:
+    print("Unsuccessfully written")
+print("Success")
