c0d59c88f12fc00a3314bd7c9deed800f32b3ce8
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-09-17 00:59:05 +0200

    calculations related data cleared

--- project/calculations/calculation.py
+++ project/calculations/calculation.py
@@ -5,18 +5,13 @@ import pandas as pd
 
 def main():
     result = get_result()
-    df = pd.DataFrame(result)
-    result_4_rooms = df[(df["rooms"] == 4) & (df["rent-full"] >= 4000)].describe()
-    # result_4_rooms = result_4_rooms.reset_index()
-    print(result_4_rooms.loc["mean"].to_dict())
-    variable_x = result_4_rooms.loc["mean"].reset_index()
-    variable_x.columns = ["asd", "3"]
-    variable_y = variable_x.copy()
-    variable_y.columns = ["asd", "4"]
-    # print(variable_y)
-    merge = variable_x.merge(variable_y, how="inner", on=["asd"])
-    print(merge)
-    print((merge["3"] - merge["4"]) / merge["3"])
+    data_frame = pd.DataFrame(result)
+
+
+def price_for_room(result):
+    data_frame = pd.DataFrame(result)
+    for _ in range(1, 5):
+        data_frame
 
 
 def get_result():
