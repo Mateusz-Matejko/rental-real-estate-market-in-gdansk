d85f017eca586357c52a85f662602936367a96f0
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-09-20 21:39:49 +0200

    data organisation within data collected sep 3 changed

--- project/collected september 3rd/original-data/clear.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("result-backup-3.json", "r") as json_file:
-    data = json.load(json_file)
-
-with open("result-backup-3.json", "w") as json_file:
-    json.dump(data, json_file, indent=2)
