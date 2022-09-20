f6ea5560a4e6a8f26aed4617db72750be433a64d
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-09-20 21:39:11 +0200

    file to organise data structures

---
+++ project/calculations/organiser-file.py
@@ -0,0 +1,23 @@
+import json
+
+# # ALL DATA CREATION
+# list_of_file = ["sep3.json", "sep11.json", "sep19.json"]
+# direction_file = "all-data.json"
+# final_result = []
+# for file in list_of_file:
+#     with open(file, "r") as file:
+#         result = json.load(file)
+#         for listing in result:
+#             final_result.append(listing)
+#
+
+# ALL DATA OPERATION
+# with open("all-data.json", "r") as file:
+#     result = json.load(file)
+
+
+
+# with open("all-data.json", "w") as f:
+#     json.dump(result, f, indent=2, sort_keys=True)
+
+
