5f980809569fd64efe31ed118296fb6f9a37542a
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-09-20 21:37:05 +0200

    helping files to orient withing clearing data and it's organisation

--- project/calculations/template-of-listing.py
+++ project/calculations/template-of-listing.py
@@ -1,5 +1,5 @@
 """
-"building-type": [0 = Pozostałe, 1= Dom wolnostojący, 2=Kamienica, 3= Szeregoiwec , 4= Blok , 5= Apartamentowiec]
+"building-type": [0 = Pozostałe, 1= Dom wolnostojący, 2=Kamienica, 3= Szeregoiwec , 4= Blok , 5= Apartamentowiec, 6= Loft]
 "furnished": true, false
 "level": 0-10, if 11 is more than 10, null if no info.
 "link": some link,
@@ -13,17 +13,4 @@
 "rooms": 1-3, if 4 is 4 or more.
 "surface": square meters
 "title": "Title of listing"
-"""
-
-"MORE IN "
-"/Users/mateusz/Documents/Code/Final Project/project/"
-"""
-Procedure of cleaning: All in 
-1. STRIP IT
-1. "rent", "negotiable"
-2. Listing clear/ keys / negotiable/ private
-3. level, rent-full, private, rent-extra
-4. furnished, surface, rooms
-5. counter, repetition
-6. Publish date
 """
