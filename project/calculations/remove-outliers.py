2a441f61ce9b6e492f465bd35da2c33ff29cbd20
Mateusz Matejko <matejko.mateusz7456@gmail.com>
2022-10-19 19:48:04 +0200

    project for FC finished

--- project/calculations/remove-outliers.py
+++ project/calculations/remove-outliers.py
@@ -11,23 +11,20 @@ def main():
     index_list = []
     for room in range(1, 5):
         df_raw = pd.DataFrame(result)
-        print()
-        print(f"Rooms: {room}")
         df_raw = df_raw[df_raw.rooms == room]
         # print(df_raw.describe())
         plot_boxplot(df_raw, "rent_extra")
         # df_raw = df_raw[(df_raw.rent_extra != df_raw.rent)]
-        for feature in ["rent", "rent_full", "rent_extra"]:
+        for feature in ["rent", "rent_full", "rent_extra", "surface"]:
             index_list.extend(outliers(df_raw, feature))
     index_list = sorted(set(index_list))
-    print(index_list)
-    print(len(index_list))
-    cleared_results = remove_outliers_by_listing_no(index_list)
-    cleared_results = clear_listing_no_after_clearing_data(cleared_results)
-    new_df = pd.DataFrame(cleared_results)
-
-    print(new_df.rent_extra.max())
-    # save_results(cleared_results, path="sample_cleared_results.json")
+    cleared_results = remove_outliers(df, index_list)
+    cleared_results_dict = clear_listing_no_after_clearing_data(cleared_results)
+    for listing in cleared_results_dict:
+        listing["price_per_sq_meter"] = round(listing["rent"] / listing["surface"], 2)
+    new_df = pd.DataFrame(cleared_results_dict)
+    print(len(cleared_results_dict))
+    save_results(cleared_results_dict, path="sample_cleared_results.json")
 
 
 def get_result(file):
@@ -42,15 +39,15 @@ def plot_boxplot(df, ft):
 
 
 def outliers(df, ft):
-    Q1 = df[ft].quantile(0.15)
-    Q3 = df[ft].quantile(0.65)
+    Q1 = df[ft].quantile(0.30)
+    Q3 = df[ft].quantile(0.70)
     IQR = Q3 - Q1
 
     # cet the lower and upper bound
     lower_bound = Q1 - 1.5 * IQR
     upper_bound = Q3 + 1.5 * IQR
 
-    ls = df.listing_no[(df[ft] < lower_bound) | (df[ft] > upper_bound)]
+    ls = df.index[(df[ft] < lower_bound) | (df[ft] > upper_bound)]
     return ls
 
 
@@ -72,12 +69,12 @@ def remove_outliers_by_listing_no(ls):
 
 def clear_listing_no_after_clearing_data(results):
     counter = 1
+    results = results.T.to_dict().values()
+    results = list(results)
     for listing in results:
         listing["listing_no"] = counter
         counter += 1
     return results
-
-
 
 
 def save_results(results, path):
